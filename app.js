12cedf54d9b91a993469f23abeb8b47cc1a822a0
wozien <571455773@qq.com>
2019-09-03 00:58:21 +0800

    audio

---
+++ app.js
@@ -0,0 +1,8 @@
+App({
+  globalData: {
+    // 播放音乐的文章id
+    playPostId: '',
+    // 是否音乐正在播放
+    playing: false
+  }
+})
--- pages/detail/post-detail.js
+++ pages/detail/post-detail.js
@@ -1,4 +1,5 @@
 const mockData = require('../../data/post-data.js');
+const app = getApp();
 
 // 文章收藏对象的缓存key
 const POST_COLLECTED = 'POST_COLLECTED';
@@ -11,7 +12,9 @@ Page({
     // 文章详情数据
     postData: {},
     // 当前文章是否收藏
-    collected: false
+    collected: false,
+    // 音乐播放状态
+    playing: false
   },
 
   /**
@@ -36,6 +39,33 @@ Page({
     } else {
       wx.setStorageSync(POST_COLLECTED, {})
     }
+
+    if(app.globalData.playPostId === postId && app.globalData.playing) {
+      this.setData({
+        playing: true
+      })
+    }
+
+    this.onAudioOperate();
+  },
+
+  onAudioOperate: function() {
+    const self = this;
+    // 音乐播放监听
+    wx.onBackgroundAudioPlay(function () {
+      self.setData({
+        playing: true
+      });
+      app.globalData.playing = true;
+      app.globalData.playPostId = self.data.postId;
+    })
+
+    wx.onBackgroundAudioPause(function () {
+      self.setData({
+        playing: false
+      })
+      app.globalData.playing = false;
+    })
   },
 
   onCollected: function() {
@@ -45,5 +75,47 @@ Page({
     // 修改缓存
     postCollected[this.data.postId] = collected
     wx.setStorageSync(POST_COLLECTED, postCollected)
-  } 
+    // 显示提示框
+    wx.showToast({
+      title: collected ? '收藏成功' : '取消成功',
+    })
+  },
+
+  onShare: function() {
+    const itemList = [
+      '分享到朋友圈',
+      '分享到好友',
+      '分享到qq'
+    ]
+
+    wx.showActionSheet({
+      itemList,
+      itemColor: "#405f80",
+      success: function(res) {
+        if(res.tapIndex !== undefined) {
+          wx.showModal({
+            title: itemList[res.tapIndex],
+            content: '是否把该文章' + itemList[res.tapIndex]
+          })
+        }
+      }
+    })
+  },
+
+  onPlayMusic: function() {
+    if(this.data.playing) {
+      wx.pauseBackgroundAudio();
+    } else {
+      const music = this.data.postData.music;
+      wx.playBackgroundAudio({
+        dataUrl: music.url,
+        title: music.title,
+        coverImgUrl: music.coverImg
+      })
+    }
+
+    this.setData({
+      playing: !this.data.playing
+    })
+  }
 })
